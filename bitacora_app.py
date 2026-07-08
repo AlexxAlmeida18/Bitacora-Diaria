6b3cae71cc7ce4b457822b6828dc93629a60e306
Alexander Almeida <xander.almeyda18@gmail.com>
2026-07-08 15:10:17 -0500

    Sube version a 1.5.0

--- bitacora_app.py
+++ bitacora_app.py
@@ -212,7 +212,7 @@ def save_json(path, data):
         json.dump(data, f, ensure_ascii=False, indent=2)
 
 
-APP_VERSION = "1.4.1"
+APP_VERSION = "1.5.0"
 UPDATE_REPO = "AlexxAlmeida18/Bitacora-Diaria"
 
 NOTIF_TASK_NAME = "BitacoraDiaria Recordatorios"
